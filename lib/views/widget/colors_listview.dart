8d8e501bb8a086c277aa2f8af6ba4d0d910851f9
reham1414 <rehamhelal981@gmail.com>
2023-09-11 19:24:29 +0300

    colors listview part two

--- lib/views/widget/colors_listview.dart
+++ lib/views/widget/colors_listview.dart
@@ -1,27 +1,62 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
-
+  const ColorItem({super.key, required this.isActive, required this.color});
+final bool isActive;
+final Color color;
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
+    return isActive ?
+     CircleAvatar(
       radius: 38,
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.white,
+      child: CircleAvatar(
+        radius: 36,
+        backgroundColor: color,
+      ),
+    )
+        :  CircleAvatar(
+      radius: 38,
+      backgroundColor: color,
     );
   }
 }
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
 
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+  List<Color> colors = [
+    const Color(0xFF1D0C12),
+    const Color(0xFF662432),
+    const Color(0xFFB53643),
+    const Color(0xFFDB7070),
+    const Color(0xFFF1C2BB),
+  ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 38 * 2,
       child: ListView.builder(
-          itemCount: 10,
+          itemCount: colors.length,
           itemBuilder: (context , index){
-            return const ColorItem();
+            return  Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 6.0),
+              child: GestureDetector(
+                  onTap: (){
+                    currentIndex = index ;
+                    setState(() {
+
+                    });
+                  },
+                  child: ColorItem(
+                    isActive: currentIndex ==index,
+                    color: colors[index],)),
+            );
           }),
     );
   }
